51cb1958381417aa9af87dcff9a83074bf3c0e53
Shaun Donnelly <shaun.donnelly@ensighten.com>
2016-05-21 13:07:54 +0100

    Removed checked in deps, added LESS compilation.

--- web-app/src/scripts/main.js
+++ web-app/src/scripts/main.js
@@ -8,31 +8,3 @@ socket.on('TfLArrivals', function(data) {
 socket.on('TfLDisruptions', function(data) {
   console.log(data);
 });
-
-var WeatherData = React.createClass({
-  getInitialState: function() {
-    return {data:{}};
-  },
-  componentDidMount: function() {
-    var that = this;
-    socket.on('weather', function(weatherData) {
-      //console.log(weatherData);
-      that.setState({data:weatherData});
-    });
-  },
-  sanitiseData: function(data) {
-
-  },
-  render: function() {
-    return (
-      <dl>
-        <dt>Weather</dt>
-        <dd>{this.state.data.toString()}</dd>
-      </dl>
-    );
-  }
-});
-ReactDOM.render(
-  <WeatherData />,
-  document.getElementById('weather')
-);
